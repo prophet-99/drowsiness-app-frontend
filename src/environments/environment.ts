0782112fff7e0a6526a43265cad4ef86d29baca3
prophet-99 <alexanderavilab@gmail.com>
2023-09-16 20:19:38 -0500

    feat: add node server to serve angular app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  API_BASE_URL: 'drowsiness-app-backend-production.up.railway.app/api/v1',
+  API_BASE_URL:
+    'https://drowsiness-app-backend-production.up.railway.app/api/v1',
   GOOGLE_API_KEY: 'AIzaSyC8wvWbR2DvpUSTIk14pICq--GW6AYXSrE',
 };
